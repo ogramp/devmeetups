e13546a72a9cb69140d80de7215385276dd45b4b
Margo Pruul <margo.pruul@gmail.com>
2017-09-05 20:28:23 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-app>
+    <v-navigation-drawer v-model="sideNav">
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>supervisor_account</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>View Meetups</v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>accessible</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>Button 2</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar dark class="primary">
+    <v-toolbar-side-icon 
+      @click.native.stop="sideNav=!sideNav"
+      class="hidden-sm-and-up"></v-toolbar-side-icon>
+      <v-toolbar-title>DevMeetup</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn flat>
+          <v-icon dark left>supervisor_account</v-icon>
+          View Meetups
+        </v-btn>
+        <v-btn flat>
+          <v-icon dark left>accessible</v-icon>
+          Button 2
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <main>
+      
+    </main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        sideNav: false
+      }
+    }
+  }
+</script>
+
+<style lang="stylus">
+  @import './stylus/main'
+</style>
